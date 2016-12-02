1d9390f50d4e19ca35cffca1f02ba035ebded426
RomeroBarata <romero.barata@gmail.com>
2016-12-01 23:27:58 -0300

    Exercises

---
+++ R/exercicios-modelagem.R
@@ -0,0 +1,25 @@
+## Exercício 01 ----------------------------------------------------------------
+# Modifique a substituição de NAs na base Titanic na função organizeData() 
+# utilizando as técnicas aprendidas na aula de análise dos dados.
+
+## Exercício 02 ----------------------------------------------------------------
+# Investigue a função factor() e modifique as conversões para factor na função 
+# organizeData() para utilizarem nomes mais expressivos aos fatores. Confira 
+# também a função recode_factor() do dplyr.
+
+## Exercício 03 ----------------------------------------------------------------
+# Treine um modelo e faça sua primeira submissão ao Kaggle! =D
+
+## Exercício 04 ----------------------------------------------------------------
+# O que poderiamos fazer para melhorar nosso modelo? Novas features? Outros 
+# classificadores? Outro pré-processamento dos dados?
+
+## Exercício 05 ----------------------------------------------------------------
+# Confira a lista de classificadores disponíveis no caret em:
+# http://topepo.github.io/caret/available-models.html
+# Treine mais dois modelos e compare a performance dos três modelos agora 
+# disponíveis.
+
+## Exercício 06 ----------------------------------------------------------------
+# Crie um registro no conjunto de teste para você, e utilizando o seu melhor 
+# modelo descubra se você sobreviveria ao desastre do Titanic! =O
